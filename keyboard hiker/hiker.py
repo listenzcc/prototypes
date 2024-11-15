d6f632b43ddc25629e95e1c68c656e4f48f75f54
listenzcc <cc.public@outlook.com>
2024-11-15 15:20:15 +0800

    Develop keyboard hiker

--- keyboard hiker/hiker.py
+++ keyboard hiker/hiker.py
@@ -18,8 +18,13 @@ Functions:
 
 # %% ---- 2024-11-15 ------------------------
 # Requirements and constants
+import tkinter
+from tkinter import ttk
+
+import sys
 import keyboard
 import argparse
+
 from loguru import logger
 from rich import print, inspect
 
@@ -29,9 +34,47 @@ logger.add('log/keyboard hiker.log', rotation='5 MB')
 # Function and class
 
 
+class GUI(tkinter.Tk):
+    label = None
+    detail = None
+    font_family = 'Courier'  # 'Helvetica'
+
+    def __init__(self):
+        super().__init__()
+        self.geometry('300x400')
+        frm = ttk.Frame(self, padding=20)
+        frm.pack()
+        title = ttk.Label(
+            frm, text='Latest key event', font=self.font_family + ' 12 bold')
+        title.pack()
+
+        label = ttk.Label(
+            frm, text='[...]', font=self.font_family+' 8 bold', justify='center',
+            relief=tkinter.GROOVE
+        )
+        label.pack(pady=20, padx=20)
+
+        var = tkinter.StringVar()
+        var.set(dir(tkinter))
+        detail = tkinter.Listbox(frm, listvariable=var, selectmode='browse')
+        detail.pack()
+
+        self.label = label
+        self.detail = detail
+        logger.debug('Initialized')
+
+
 class KeyboardHiker(object):
-    verbose = False
-    suppress = False
+    # Flags
+    verbose: bool = False
+    suppress: bool = False
+    no_gui: bool = False
+
+    # Variables
+    namespace = None
+    escape_key_name: str = None
+    gui: GUI = None
+    count: int = 0
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -39,36 +82,85 @@ class KeyboardHiker(object):
             logger.debug(f'Set {k} as {v}')
         logger.info('Initialized')
 
-    def wait_until(self, key_name):
-        logger.info(f'Waiting for {key_name}')
-        keyboard.wait(key_name, suppress=self.suppress)
-        logger.info(f'Key pressed: {key_name}')
+    def wait_until(self):
+        escape_key_name = self.escape_key_name
+        logger.info(f'Waiting for escape key: {escape_key_name}')
+
+        # Wait until the escape key is pressed,
+        # the block is executed by keyboard.wait method
+        if self.no_gui:
+            keyboard.wait(escape_key_name, suppress=self.suppress)
+
+        # the block is executed by gui.mainloop method
+        else:
+            self.gui = GUI()
+            if escape_key_name:
+                keyboard.on_press_key(
+                    escape_key_name, lambda x: self.gui.destroy(), suppress=self.suppress)
+
+            # Make the content of the list box
+            lst = [f'{e[0]}: {e[1]}' for e in self.namespace._get_kwargs()]
+            if self.suppress and self.escape_key_name is not None:
+                lst.append(
+                    'Warning: escape key will be ignored since it is suppressed.')
+
+            # Make the string var
+            var = tkinter.StringVar()
+            var.set(lst)
+
+            # Update the detail listbox
+            self.gui.detail.config(listvariable=var)
+
+            # Run the mainloop
+            self.gui.mainloop()
+
+        logger.info(f'Escape key pressed: {escape_key_name}')
+        return 0
 
     def bind_on_press(self):
         keyboard.on_press(self.callback, suppress=self.suppress)
         logger.debug(f'Bound with onPress event, suppress={self.suppress}')
 
     def callback(self, event):
+        self.count += 1
+
         if self.verbose:
             inspect(event)
+
+        if self.gui:
+            self.gui.label.config(
+                text=f'{event}\n{event.time}\nCount: {self.count}')
+
         logger.debug(f'Got key press: {event}, {event.time}')
 
 
 # %% ---- 2024-11-15 ------------------------
 # Play ground
 if __name__ == "__main__":
+    # Arguments
     parser = argparse.ArgumentParser(description='Keyboard hiker application')
-    parser.add_argument('-w', '--wait-key', help='Wait for key to stop')
-    parser.add_argument('-v', '--verbose', help='Verbose key press',
-                        action='store_true')
-    parser.add_argument('-s', '--suppress', help='Suppress the key press',
-                        action='store_true',)
+    parser.add_argument('-e', '--escape-key-name',
+                        help='Escape from the app if the given key is pressed')
+    parser.add_argument('-v', '--verbose',
+                        help='Verbose key press', action='store_true')
+    parser.add_argument('-s', '--suppress',
+                        help='Suppress the key press', action='store_true')
+    parser.add_argument('-n', '--no-gui',
+                        help='Not using the GUI', action='store_true')
     namespace = parser.parse_args()
     print(namespace)
 
-    kr = KeyboardHiker(suppress=namespace.suppress, verbose=namespace.verbose)
+    # Keyboard hiker
+    kwargs = dict(
+        namespace=namespace,
+        suppress=namespace.suppress,
+        verbose=namespace.verbose,
+        no_gui=namespace.no_gui,
+        escape_key_name=namespace.escape_key_name,
+    )
+    kr = KeyboardHiker(**kwargs)
     kr.bind_on_press()
-    kr.wait_until(namespace.wait_key)
+    sys.exit(kr.wait_until())
 
 
 # %% ---- 2024-11-15 ------------------------
