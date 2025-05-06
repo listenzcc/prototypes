191dda23f314b60bd84d6f614c4478c57e17e143
listenzcc <cc.public@outlook.com>
2025-05-06 17:32:03 +0800

    Refactor logging setup to remove debug log, add error handling, and enhance info logging

--- logging rules/main.py
+++ logging rules/main.py
@@ -1,9 +1,27 @@
 import sys
+import logging
 from loguru import logger
 
 logger.remove()
+
+# Both info and above
 logger.add(sys.stderr, level='INFO')
-# logger.add('debug.log', level='DEBUG')
+
+# Only info
+logger.add(
+    'info.pure.log', filter=lambda record: record["level"].no < logging.WARNING, level='INFO')
+
+# Both info and above
+logger.add(
+    'info.log', level='INFO')
+
+# Warning and above
+logger.add('error.log', level='WARNING')
 
 logger.debug('debug')
 logger.info('info')
+
+try:
+    1/0
+except Exception as err:
+    logger.opt(exception=True).error(err)
